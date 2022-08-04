a67c28f0f2f3ee9a8253d7d583a0b56eb79032a3
SebastianRaiquenParisi <sebastianrparisi@gmail.com>
2022-08-04 19:46:24 -0300

    client returning a JWToken, making another server aside from .NETCore application to test functionalities

---
+++ server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const PORT = ":9000"
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Super Secret Information")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(PORT, nil))
+}
+
+func main() {
+	fmt.Println("My Simple Server")
+	handleRequests()
+}
